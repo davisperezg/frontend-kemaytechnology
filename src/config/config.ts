3b4b4d76e9d6884ee27faefe892b8556b30ee87b
Davis Keiner Pérez Guzmán <davisperezg@gmail.com>
2021-08-09 02:13:50 -0500

    build subido

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,4 +1,6 @@
 import * as dotenv from "dotenv";
 dotenv.config({ path: __dirname + "/.env" });
 
-export const URLC = process.env.URL || "http://localhost:3000/graphql";
+console.log(process.env.REACT_APP_URL);
+export const URLC =
+  process.env.REACT_APP_URL || "http://localhost:3000/graphql";
